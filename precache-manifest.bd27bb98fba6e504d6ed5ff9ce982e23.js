fded0bea6db582ad018b90c98d3ecae835037294
Maks <makssenuk2002@gmail.com>
2021-12-06 20:30:18 +0300

    Initial dist subtree commit

---
+++ precache-manifest.bd27bb98fba6e504d6ed5ff9ce982e23.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06717679193a2756ac1e",
+    "url": "/vueAppTodo/css/app.04b8991e.css"
+  },
+  {
+    "revision": "f10aa4f20e578767c9ff",
+    "url": "/vueAppTodo/css/chunk-vendors.31dd1ebd.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/vueAppTodo/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/vueAppTodo/fonts/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/vueAppTodo/fonts/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/vueAppTodo/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/vueAppTodo/fonts/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/vueAppTodo/fonts/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/vueAppTodo/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/vueAppTodo/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/vueAppTodo/fonts/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/vueAppTodo/fonts/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/vueAppTodo/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/vueAppTodo/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/vueAppTodo/img/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/vueAppTodo/img/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/vueAppTodo/img/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "a3e0c313c5439eab2068b2b6c954ca6f",
+    "url": "/vueAppTodo/img/Фон.a3e0c313.jpg"
+  },
+  {
+    "revision": "ebeebc0f2f11ff3ed2a67974df7c7c9c",
+    "url": "/vueAppTodo/index.html"
+  },
+  {
+    "revision": "06717679193a2756ac1e",
+    "url": "/vueAppTodo/js/app.4c9b0066.js"
+  },
+  {
+    "revision": "f10aa4f20e578767c9ff",
+    "url": "/vueAppTodo/js/chunk-vendors.87194b10.js"
+  },
+  {
+    "revision": "f89eae5c3c4e7439a9401f6cbf9c5e32",
+    "url": "/vueAppTodo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueAppTodo/robots.txt"
+  }
+]);
